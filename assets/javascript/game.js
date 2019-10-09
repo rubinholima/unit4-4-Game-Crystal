b8c928e23f8929378697180105adb3423d93d12d
Rubinho Lima <rubinholima@me.com>
2019-10-09 07:44:48 -0400

    Working Javascript File! Creating an array.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,37 +8,54 @@ window.addEventListener("DOMContentLoaded", function (event) {
 let magNum = Math.floor(Math.random() * 120) + 19;
 $('#magicNumber').text(magNum)
 
-let scoreGem = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-let scoreG1 = 0;
-let scoreG2 = 0;
-let scoreG3 = 0;
-let scoreG4 = 0;
+$(function () {
 
-function score() {
-    scoreG1 = Math.floor(Math.random() * scoreGem.length);
-    scoreG2 = Math.floor(Math.random() * scoreGem.length);
-    scoreG3 = Math.floor(Math.random() * scoreGem.length);
-    scoreG4 = Math.floor(Math.random() * scoreGem.length);
-    if (scoreG1 === scoreG2) {
-        scoreG2 = scoreG2++
-    } else if (scoreG2 === scoreG3) {
-        scoreG3 = scoreG3++
-    } else if (scoreG3 === scoreG4) {
-        scoreG3 = scoreG4++
+    let scoreGem = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+    let scoreG = [];
+
+    function score() {
+
+        const sizeScore = 5;
+        const gemSizeNumber = 1
+
+        for (let i = 0; i < sizeScore; i++) {
+            const scoreMath = Math.floor(Math.random() * scoreGem.length);
+            scoreG.push(scoreMath)
+            if (scoreG[i] !== scoreG[i + 1]) {
+                scoreMath++;
+            }
+        }
+
+        
+
+
+
+
+
+
+
+        // if (scoreG1 === scoreG2) {
+        //     scoreG2 += scoreG2++;
+        // } else if (scoreG2 === scoreG3) {
+        //     scoreG3 += scoreG3++;
+        // } else if (scoreG3 === scoreG4) {
+        //     scoreG3 += scoreG4++;
+        // }
+
     }
+    score();
 
-}
-score();
+    $('#gem1').click(function (event) {
+        $('#score').text(scoreG1);
+    })
+    $('#gem2').click(function (event) {
+        $('#score').text(scoreG2);
+    })
+    $('#gem3').click(function (event) {
+        $('#score').text(scoreG3);
+    })
+    $('#gem4').click(function (event) {
+        $('#score').text(scoreG4);
+    })
 
-$('#gem1').click(function (event) {
-    $('#score').text(scoreG1);
-})
-$('#gem2').click(function (event) {
-    $('#score').text(scoreG2);
-})
-$('#gem3').click(function (event) {
-    $('#score').text(scoreG3);
-})
-$('#gem4').click(function (event) {
-    $('#score').text(scoreG4);
 })
