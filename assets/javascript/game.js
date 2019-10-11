c6af69b709531fa456fa0af5962e04abc2c847a0
Rubinho Lima <rubinholima@me.com>
2019-10-11 11:14:21 -0400

    Final Adjustments Assignment week 4

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,6 +3,7 @@ $(document).ready(function () {
         console.log("DOM fully loaded and parsed");
     });
 
+    // Initializing the Variables!
 
     let scoreGem = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
     let scoreG = [];
@@ -14,7 +15,9 @@ $(document).ready(function () {
     let count = 0;
     let sizeCoun = 1;
     let gem = 0;
-
+    let audioElement = "";
+    let audioWin = "";
+    let audioLoss = "";
 
 
     // Loading the Magic Number
@@ -22,6 +25,8 @@ $(document).ready(function () {
     $('#magicNumber').text(magNum)
 
     score();
+
+    // function thats populate the array and calls the function egualsFunction() that test if there any eguals number in the array.
 
     function score() {
 
@@ -51,73 +56,92 @@ $(document).ready(function () {
             }
         }
 
-        gems();
+    }
+    // Audio elements
+
+    audioElement = document.createElement("audio");
+    audioElement.setAttribute("src", "./assets/Sounds Effects/Laser hit.mp3");
+
+    audioWin = document.createElement("audio");
+    audioWin.setAttribute("src", "./assets/Sounds Effects/Win Fanfare.mp3");
+
+    audioLoss = document.createElement("audio");
+    audioLoss.setAttribute("src", "./assets/Sounds Effects/Egyptian Game Over.mp3");
+
+    // Click Event Listeners
+
+    $('#gem1').click(function (event) {
+        gem = 0
+        totalScore = finalTotal(totalScore);
+
+    })
+    $('#gem2').click(function (event) {
+        gem = 1
+        totalScore = finalTotal(totalScore);
+    })
+    $('#gem3').click(function (event) {
+        gem = 2
+        totalScore = finalTotal(totalScore);
+    })
+    $('#gem4').click(function (event) {
+        gem = 3
+        totalScore = finalTotal(totalScore);
+    })
+
+    // Increment the value of Gems and tests if theres a win or loss
+
+    function finalTotal() {
+
+        totalScore += scoreG[gem];
+
+        audioElement.play();
+
+        $('#score').text(totalScore);
+        if (totalScore === magNum) {
+            winner++;
+            win();
+
+        } else if (totalScore > magNum) {
+            loser++;
+            loss();
+        }
+        return totalScore;
     }
 
-    function gems() {
-        $('#gem1').click(function (event) {
-            gem = 0
-            totalScore = finalTotal(totalScore);
-        })
-        $('#gem2').click(function (event) {
-            gem = 1
-            totalScore = finalTotal(totalScore);
-        })
-        $('#gem3').click(function (event) {
-            gem = 2
-            totalScore = finalTotal(totalScore);
-        })
-        $('#gem4').click(function (event) {
-            gem = 3
-            totalScore = finalTotal(totalScore);
-        })
-
-        function finalTotal() {
-
-            totalScore += scoreG[gem];
-
-            console.log(gem)
-
-            $('#score').text(totalScore);
-
-            if (totalScore === magNum) {
-                winner++;
-                win();
-
-            } else if (totalScore > magNum) {
-                loser++;
-                loss();
-            }
-            return totalScore;
-        }
-
-    };
+    // Function WIN.. calls Restart function
 
     function win() {
+        audioWin.play();
         $("#youWin").show();
-        $("#win").text('WINS ........ : ' + winner);
+        $("#gemsButton").hide();
+        $("#win").text('WINS ........ :  ' + winner);
 
         $('.btn').click(function (event) {
             restart()
         })
 
     }
+    // Function loss.. calls Restart function
 
     function loss() {
+        audioLoss.play();
         $("#youLose").show();
-        $("#loss").text('LOSSES ..... : ' + loser);
-
+        $("#gemsButton").hide();
+        $("#loss").text('LOSSES ..... :   ' + loser);
         $('.btn').click(function (event) {
             restart()
         })
 
 
     }
+
+    // Reset Game and start again
 
     function restart() {
         $("#youWin").hide();
         $("#youLose").hide();
-        $('#score').text("");
+        $('#score').text("00");
+        $("#gemsButton").show();
 
         magNum = Math.floor(Math.random() * 120) + 19;
         $('#magicNumber').text(magNum)
